42e1e649c3893b50efcea36301368db555c1a137
vagrant <vagrant@homestead>
2021-08-06 05:55:04 +0000

    update js

--- web/modules/custom/ib_task_94/js/script.js
+++ web/modules/custom/ib_task_94/js/script.js
@@ -2,7 +2,16 @@
  * @file
  * Simple JavaScript div class file.
  */
-let body = document.body;
-let text = body.className;
-let elem = document.querySelector('div')
-alert(elem.textContent);
+
+(function ($, Drupal, settings) {
+
+  Drupal.behaviors.ib_task_94 = {
+    attach: function () {
+      let body = document.body;
+      let text = body.className;
+      let elem = document.querySelector('div')
+      alert(elem.textContent = text);
+    }
+  }
+
+})(jQuery, Drupal, drupalSettings);
