57c9a0c73c6a0ed55823ab47ab214d130dca31b6
vagrant <vagrant@homestead>
2021-08-10 07:52:42 +0000

    task 33

--- web/modules/custom/ib_task_32/src/Form/IbTask32Form.php
+++ web/modules/custom/ib_task_32/src/Form/IbTask32Form.php
@@ -9,7 +9,8 @@ use Drupal\Core\Form\FormStateInterface;
  * Extends from FormBase class Form API
  * @see \Drupal\Core\Form\FormBase
  */
-class IbTask32Form extends FormBase {
+class IbTask32Form extends FormBase
+{
 
   public function buildForm(array $form, FormStateInterface $form_state) {
     $city_terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('citylist');
@@ -25,17 +26,42 @@ class IbTask32Form extends FormBase {
       $country[$country_term->tid] = $country_term->name;
     }
 
-    $form['city'] = [
-      '#type' => 'select',
-      '#options' => $city,
-      '#title' => $this->t('City'),
-    ];
-
     $form['country'] = [
       '#type' => 'select',
+      '#empty_value' => '',
+      '#empty_option' => '- Select a value -',
+      '#default_value' => (isset($values['country']) ? $values['country'] : ''),
       '#options' => $country,
       '#title' => $this->t('Country'),
+      '#ajax' => array(
+        'callback' => [$this, 'mySelectChange'],
+        'event' => 'change',
+        'wrapper' => 'edit-city',
+      ),
     ];
+
+    $form['city'] = [
+      '#type' => 'select',
+      '#options' => ['Choose the country'],
+    ];
+
+    $values = $form_state->getValues();
+    if (!empty($values['country'])) {
+    $cities = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
+      ->loadByProperties(['vid' => 'citylist', 'field_country' => $values['country']]);
+    $cities_of_country = [];
+
+      foreach ($cities as $city_id) {
+        foreach ($city as $key => $value) {
+          if ($city_id->id() == $key) {
+            $cities_of_country[] = $value;
+          }
+        }
+      }
+
+      $form['city']['#title'] = 'City';
+      $form['city']['#options'] = $cities_of_country;
+    }
 
     $form['actions']['submit'] = [
       '#type' => 'submit',
@@ -45,12 +71,17 @@ class IbTask32Form extends FormBase {
     return $form;
   }
 
+  public function mySelectChange(array &$form, FormStateInterface $form_state) {
+    return $form['city'];
+  }
+
   public function getFormId() {
     return 'ib_task_32_form';
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $city_key = $form_state->getValue('city');
+
     $country_key = $form_state->getValue('country');
     \Drupal::logger('ib_task_32')->notice('City - @city Country - @country.',
 
@@ -60,3 +91,5 @@ class IbTask32Form extends FormBase {
       ]);
   }
 }
+
+
