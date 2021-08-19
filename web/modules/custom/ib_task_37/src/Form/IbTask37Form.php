f2a7191aab7a7e944d420df977e01ef0d88124a0
IvanBrunevskiy <brunevskiyivan@gmail.com>
2021-08-19 10:50:43 +0300

    task 37

--- web/modules/custom/ib_task_37/src/Form/IbTask37Form.php
+++ web/modules/custom/ib_task_37/src/Form/IbTask37Form.php
@@ -4,7 +4,7 @@ namespace Drupal\ib_task_37\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\node\Entity\Node;
+use Drupal\user\Entity\User;
 
 /**
  * Extends from FormBase class Form API
@@ -14,67 +14,84 @@ class IbTask37Form extends FormBase {
 
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-//    $form['file'] = [
-//      '#title' => $this->t('CSV file'),
-//      '#type' => 'managed_file',
-//      '#upload_location' => 'public://',
-//      '#upload_validators' => array(
-//        'file_validate_extensions' => array('csv'),
-//      ),
-//      '#required' => TRUE,
-//    ];
-    $form['submit'] = array(
-      '#type' => 'submit',
-      '#value' => 'Начать',
-    );
+    $form['#attributes']['enctype'] = "multipart/form-data";
 
+    $form['csvfile'] = [
+      '#title' => $this->t('CSV file'),
+      '#type' => 'managed_file',
+      '#upload_location' => 'public://',
+      '#upload_validators' => [
+        'file_validate_extensions' => ['csv'],
+      ],
+      '#required' => TRUE,
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => 'Import',
+    ];
 
     return $form;
   }
 
-  public function getFormId() {
+  public function getFormId()
+  {
     return 'ib_task_37_form';
   }
 
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
-    $query = \Drupal::database()->select('node_field_data', 'nfd');
-    $query->fields('nfd', ['nid']);
-    $result = $query->execute()->fetchAll();
-    $operations = [];
-    foreach ($result as $row) {
-      $operations[] = array('ib_task_37_change_date', [$row->nid]);
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $file = \Drupal::entityTypeManager()->getStorage('file')
+      ->load($form_state->getValue('csvfile')[0]);
+    $full_path = $file->get('uri')->value;
+    $users_info = [];
+
+    if (($file_read = fopen($full_path, 'r')) !== FALSE) {
+
+      while (($data = fgetcsv($file_read, 1000, ',')) !== FALSE) {
+        $users_info[] = $data;
+      }
+      fclose($file_read);
     }
-    $batch = array(
+
+    $operations = [];
+
+    foreach ($users_info as $row) {
+      $operations[] = [
+        [$this, 'ib_task_37_create_users'],
+        [$row],
+      ];
+    }
+
+    $batch = [
       'operations' => $operations,
-      'finished' => 'ib_task_37_batch_finished',
-      'title' => 'Обновление дат',
-      'init_message' => 'Подготовка данных',
-      'progress_message' => 'Выполнено @current из @total.',
-      'error_message' => 'Произошла ошибка.',
-    );
+      'finished' => [$this, 'ib_task_37_batch_finished'],
+      'title' => 'Users loading',
+      'init_message' => 'Data preparation',
+      'progress_message' => 'Completed @current из @total.',
+      'error_message' => 'An error has occurred.',
+    ];
     batch_set($batch);
-
   }
-    function ib_task_37_change_date($nid)
-    {
 
-      $node = Node::load($nid);
-      $node->get('created')->value = 1628845111;
-      $node->save();
-
+    function ib_task_37_create_users($user_info) {
+      $str = strpos($user_info[0], '@');
+      $user_name = substr($user_info[0], 0, $str);
+      User::create([
+        'name' => $user_name,
+        'mail' => $user_info[0],
+        'pass' => $user_info[1],
+      ])->save();
     }
 
     /**
      * Batch finish callback.
      */
-    function ib_task_37_batch_finished($success, $results, $operations)
-    {
+    function ib_task_37_batch_finished($success) {
       if ($success) {
-        \Drupal::messenger()->addMessage('Обновлена дата у материалов');
-      } else {
-        \Drupal::messenger()->addMessage('Завершено с ошибками.');
+        \Drupal::messenger()->addMessage('Users uploaded successfully');
+      }
+      else {
+        \Drupal::messenger()->addMessage('Completed with errors.');
       }
     }
-
 }
